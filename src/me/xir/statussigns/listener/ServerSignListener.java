6c101a83b8765beb843cc501d956079142cdd1e1
Cyberpew <root@cyberpew.me>
2014-01-14 14:50:59 -0800

    Testing if these strings are working.

--- src/me/xir/statussigns/listener/ServerSignListener.java
+++ src/me/xir/statussigns/listener/ServerSignListener.java
@@ -1,17 +1,7 @@
 package me.xir.statussigns.listener;
 
-import com.google.gson.Gson;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-
 import me.xir.statussigns.StatusSigns;
 
-import org.bukkit.craftbukkit.libs.com.google.gson.Gson;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 
@@ -20,35 +10,13 @@ public class ServerSignListener implements Listener {
 	private StatusSigns plugin;
 	
 	public void onSignChange(SignChangeEvent e) {
-		String servername = plugin.getConfig().getString("serversigns.server.name");
+		String servername = plugin.getConfig().getString("serversigns.servers" + e.getLine(1));
+		String serverip = plugin.getConfig().getString(servername + "ip");
 		if (e.getLine(0).equalsIgnoreCase("[ServerSigns]")) {
 			if (e.getLine(1).equalsIgnoreCase(servername)) {
-				
-				HashMap JSON = pingServer(server);
-				
-				if (JSON == null || JSON.get("status").equals("false")) {
-					e.setLine(3, "Offline");
-				} else {
-					e.setLine(3, "Online");
-				}	
+				e.setLine(3, serverip);
 			}
 		}
 	}
-	
-	public static HashMap pingServer(String serverip) {
-		try {
-			URL url = new URL("http://api.iamphoenix.me/get/?server_ip=" + serverip);
-			try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
-				String data = reader.readLine();
-				
-				Gson gson = new Gson();
-				return gson.fromJson(data,  HashMap.class);
-			}
-		} catch (MalformedURLException ex) {
-			ex.printStrackTrace();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-		return null;
-	}
+
 }
