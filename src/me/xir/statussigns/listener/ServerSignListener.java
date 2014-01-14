483e45faf33105d483543e4adc801aa7ad078621
Cyberpew <root@cyberpew.me>
2014-01-14 14:31:16 -0800

    Non-working example code for proof of concept.

--- src/me/xir/statussigns/listener/ServerSignListener.java
+++ src/me/xir/statussigns/listener/ServerSignListener.java
@@ -1,7 +1,17 @@
 package me.xir.statussigns.listener;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
 import me.xir.statussigns.StatusSigns;
 
+import org.bukkit.craftbukkit.libs.com.google.gson.Gson;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 
@@ -13,10 +23,32 @@ public class ServerSignListener implements Listener {
 		String servername = plugin.getConfig().getString("serversigns.server.name");
 		if (e.getLine(0).equalsIgnoreCase("[ServerSigns]")) {
 			if (e.getLine(1).equalsIgnoreCase(servername)) {
-				//echo servers players/online or offline to sign
 				
+				HashMap JSON = pingServer(server);
+				
+				if (JSON == null || JSON.get("status").equals("false")) {
+					e.setLine(3, "Offline");
+				} else {
+					e.setLine(3, "Online");
+				}	
 			}
 		}
 	}
-
+	
+	public static HashMap pingServer(String serverip) {
+		try {
+			URL url = new URL("http://api.iamphoenix.me/get/?server_ip=" + serverip);
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+				String data = reader.readLine();
+				
+				Gson gson = new Gson();
+				return gson.fromJson(data,  HashMap.class);
+			}
+		} catch (MalformedURLException ex) {
+			ex.printStrackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
 }
