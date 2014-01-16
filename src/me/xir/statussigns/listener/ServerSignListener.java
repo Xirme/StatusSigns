c652e598e6da44f6f01313534330eb72eabadfec
Cyberpew <root@cyberpew.me>
2014-01-15 21:24:30 -0800

    Getting somewhere I think

--- src/me/xir/statussigns/listener/ServerSignListener.java
+++ src/me/xir/statussigns/listener/ServerSignListener.java
@@ -1,8 +1,16 @@
 package me.xir.statussigns.listener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+
 import me.xir.statussigns.StatusSigns;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.craftbukkit.libs.com.google.gson.Gson;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
@@ -20,14 +28,27 @@ public class ServerSignListener implements Listener {
 		
 		// Rule one: Make a variable for everything you call more than once.
 		FileConfiguration config = plugin.getConfig();
+		String player = e.getPlayer().getName();
 		String servername = e.getLine(1);
 		
 		if (e.getLine(0).equalsIgnoreCase("[ServerSigns]")) {
 			// Rule two: Don't make a variable for anything you only call once.
 			if (config.getString("serversigns.servers." + servername) != null) {
-				e.setLine(3, config.getString("serversigns.servers." + servername + ".ip"));
-				System.out.println("A sign was registered.");
-			} System.out.println("derp1");
-		} System.out.println("derp2");
+				//do to check if offline or online
+				try { 
+					URL url = new URL("http://api.iamphoenix.me/get/?server_ip=" + config.getString("serversigns.servers." + servername + ".ip") + ":" + config.getString("serversigns.servers." + servername + ".port"));
+					try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+						String data = reader.readLine();
+						
+						Gson gson = new Gson();
+						return gson.fromJson(data,  HashMap.class);
+					}
+				} catch (MalformedURLException ex) {
+					player.sendMessage(ex.printStackTrace());
+				} catch (IOException ex) {
+					player.sendMessage(ex.printStackTrace());
+				}
+			}
+		}
 	}
 }
